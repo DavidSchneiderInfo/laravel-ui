cb1e5a88bfdc6575b5488440d352d05c62f775dc
David Schneider <kontakt@davidschneider.info>
2022-09-29 00:19:35 +0200

    Adding PackageServiceProvicer

---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DavidSchneiderInfo\LaravelUi;
+
+#use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                AuthCommand::class,
+                ControllersCommand::class,
+                UiCommand::class,
+            ]);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        #Route::mixin(new AuthRouteMethods);
+    }
+}
